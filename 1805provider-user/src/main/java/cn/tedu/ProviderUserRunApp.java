9e5b48c086ca07ac9a1dd6ffbec4120269a647d0
lifeiupup <15154777136@163.com>
2019-04-14 14:56:00 +0800

    test3

---
+++ 1805provider-user/src/main/java/cn/tedu/ProviderUserRunApp.java
@@ -0,0 +1,14 @@
+package cn.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient   //标识这个服务是Eureka客户端程序
+public class ProviderUserRunApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderUserRunApp.class, args);
+	}
+
+}
